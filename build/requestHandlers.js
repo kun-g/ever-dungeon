34645eb88c160ac6be9e7c22880bd766180ab336
Kun.G <nukoug@gmail.com>
2014-04-21 15:57:02 +0800

    Commit changes branch:develop @ 102 Server:temp Table:develop

--- build/requestHandlers.js
+++ build/requestHandlers.js
@@ -569,7 +569,7 @@
         switch (arg.stp) {
           case 'AppStore':
             options = {
-              hostname: 'buy.itunes.apple.com',
+              hostname: 'sandbox.itunes.apple.com',
               port: 443,
               path: '/verifyReceipt',
               method: 'POST'
