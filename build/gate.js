4a86423f5aa2b4168df0289d540961b5ca357a6d
Kun.G <nukoug@gmail.com>
2014-04-23 00:12:18 +0800

    Commit changes branch:master @ 85 Server:master Table:master

--- build/gate.js
+++ build/gate.js
@@ -1,96 +1,114 @@
 (function() {
-  var SimpleProtocolDecoder, SimpleProtocolEncoder, async, net, startTcpServer, _ref;
+  var SimpleProtocolDecoder, SimpleProtocolEncoder, async, net, _ref;
 
   _ref = require('./requestStream'), SimpleProtocolDecoder = _ref.SimpleProtocolDecoder, SimpleProtocolEncoder = _ref.SimpleProtocolEncoder;
+
+  require('./define');
 
   async = require('async');
 
   net = require('net');
 
-  startTcpServer = function(servers, port) {
-    var appNet;
-    appNet = {};
-    appNet.server = net.createServer(function(c) {
-      var decoder, encoder;
-      appNet.aliveConnections.push(c);
-      c.connectionIndex = appNet.aliveConnections.length - 1;
-      c.pendingRequest = new Buffer(0);
-      c.on('end', function() {
-        return delete appNet.aliveConnections[c.connectionIndex];
-      });
-      decoder = new SimpleProtocolDecoder();
-      encoder = new SimpleProtocolEncoder();
-      encoder.setFlag('size');
-      c.pipe(decoder);
-      c.decoder = decoder;
-      c.encoder = encoder;
-      c.server = appNet.createConnection();
-      encoder.pipe(c.server);
-      c.server.pipe(c);
-      decoder.on('request', function(request) {
-        if (request) {
-          if (request.CMD === 101) {
-            console.log({
-              request: request,
-              ip: c.remoteAddress
-            });
+  initGlobalConfig(null, function() {
+    var gServerConfig, gServerID, startTcpServer;
+    startTcpServer = function(servers, port) {
+      var appNet, getAliveConnection;
+      appNet = {};
+      appNet.server = net.createServer(function(c) {
+        var decoder, encoder;
+        appNet.aliveConnections.push(c);
+        c.connectionIndex = appNet.aliveConnections.length - 1;
+        c.pendingRequest = new Buffer(0);
+        c.on('end', function() {
+          return delete appNet.aliveConnections[c.connectionIndex];
+        });
+        decoder = new SimpleProtocolDecoder();
+        encoder = new SimpleProtocolEncoder();
+        encoder.setFlag('size');
+        c.pipe(decoder);
+        c.decoder = decoder;
+        c.encoder = encoder;
+        c.server = appNet.createConnection(c);
+        encoder.pipe(c.server);
+        c.server.pipe(c);
+        decoder.on('request', function(request) {
+          if (request) {
+            if (request.CMD === 101) {
+              console.log({
+                request: request,
+                ip: c.remoteAddress
+              });
+            }
+            return encoder.writeObject(request);
+          } else {
+            return c.destroy();
           }
-          return encoder.writeObject(request);
-        } else {
+        });
+        return c.on('error', function(error) {
+          console.log(error);
           return c.destroy();
-        }
-      });
-      return c.on('error', function(error) {
-        console.log(error);
-        return c.destroy();
-      });
-    });
-    appNet.backends = servers.map(function(s, id) {
-      return {
-        ip: s.ip,
-        port: s.port,
-        alive: false
-      };
-    });
-    appNet.createConnection = function() {
-      var server;
-      server = appNet.aliveServers[appNet.currIndex];
-      appNet.currIndex = appNet.currIndex + 1 % appNet.aliveServers.length;
-      return net.connect(server.port, server.ip);
-    };
-    setInterval((function() {
-      return async.map(appNet.backends, function(e, cb) {
-        var s;
-        if (!e.alive) {
-          s = net.connect(e.port, e.ip, function() {
-            e.alive = true;
-            s.destroy();
-            return cb(null, e);
-          });
-          return s.on('error', function(err) {
-            console.log('Error', err, e);
-            e.alive = false;
-            s.destroy();
-            return cb(null, e);
-          });
-        }
-      }, function(err, result) {
-        return appNet.aliveServers = result.filter(function(e) {
-          return e.alive;
         });
       });
-    }), 3000);
-    appNet.currIndex = 0;
-    appNet.aliveConnections = [];
-    appNet.server.listen(port, console.log);
-    return appNet.server.on('error', console.log);
-  };
-
-  startTcpServer([
-    {
-      ip: '10.4.4.188',
-      port: 7756
-    }
-  ], 7757);
+      appNet.backends = servers.map(function(s, id) {
+        return {
+          ip: s.ip,
+          port: s.port,
+          alive: false
+        };
+      });
+      getAliveConnection = function() {
+        var count, i, _i;
+        count = appNet.backends.length;
+        servers = appNet.backends;
+        for (i = _i = 1; 1 <= count ? _i <= count : _i >= count; i = 1 <= count ? ++_i : --_i) {
+          if (!servers[i + appNet.currIndex % servers.length].alive) {
+            continue;
+          }
+          appNet.currIndex = appNet.currIndex + 1 % appNet.aliveServers.length;
+          return servers[i + appNet.currIndex % servers.length];
+        }
+        return null;
+      };
+      appNet.createConnection = function(socket) {
+        var c, server;
+        server = getAliveConnection();
+        c = net.connect(server.port, server.ip);
+        c.on('error', function(err) {
+          c.destroy();
+          socket.destroy();
+          return c = null;
+        });
+        c.on('end', function(err) {
+          c.destroy();
+          socket.destroy();
+          return c = null;
+        });
+        return c;
+      };
+      setInterval((function() {
+        return appNet.backends.forEach(function(e) {
+          var s;
+          if (!e.alive) {
+            s = net.connect(e.port, e.ip);
+            s.on('connect', function() {
+              console.log('Connection On', e);
+              return e.alive = true;
+            });
+            s.on('end', function(err) {
+              console.log('Connection Lost', e);
+              return e.alive = false;
+            });
+            return s = null;
+          }
+        });
+      }), 10000);
+      appNet.currIndex = 0;
+      appNet.server.listen(port, console.log);
+      return appNet.server.on('error', console.log);
+    };
+    gServerID = queryTable(TABLE_CONFIG, 'ServerID');
+    gServerConfig = queryTable(TABLE_CONFIG, 'ServerConfig')[gServerID];
+    return startTcpServer(gServerConfig.Gate, 7757);
+  });
 
 }).call(this);
--- build/player.js
+++ build/player.js
@@ -371,6 +371,7 @@
           return this.handleReceipt(payment, 'AppStore', postResult);
         case 'PP25':
         case 'ND91':
+        case 'KY':
           myReceipt = payment.receipt;
           return async.waterfall([
             function(cb) {
@@ -378,7 +379,7 @@
                 if ((receipt != null) && receipt.state !== RECEIPT_STATE_DELIVERED) {
                   return cb(Error(RET_Issue37));
                 } else {
-                  return cb(null, myReceipt, paymentType);
+                  return cb(null, myReceipt, payment.paymentType);
                 }
               });
             }, (function(_this) {
