40916c9ca771d6414906fd8d36a26d8c81e0aade
Kun.G <nukoug@gmail.com>
2014-05-05 10:21:50 +0800

    Commit changes branch:master @ 86 Server:master Table:master

---
+++ build/campaign.js
@@ -0,0 +1,239 @@
+exports.data = {
+  "Charge": {
+    "show": true,
+    "title": "充值就得奖",
+      "banner":"event-banner-wzydr.png",
+    "description": "单笔充值达到6元，12元，30元，68元，128元，198元，328元，648元分别得到奖励。\n\n***奖励内容：\n\n充值6元，即可获得\n\n##[{\"type\":2,\"count\":60},{\"type\":0,\"value\":0,\"count\":5},{\"type\":0,\"value\":540,\"count\":1}]\n\n** \n充值12元，即可获得\n\n##[{\"type\":2,\"count\":130},{\"type\":0,\"value\":0,\"count\":10},{\"type\":0,\"value\":540,\"count\":2}]\n\n** \n充值30元，即可获得\n\n##[{\"type\":2,\"count\":330},{\"type\":0,\"value\":0,\"count\":20},{\"type\":0,\"value\":540,\"count\":2},{\"type\":0,\"value\":534,\"count\":3}]\n\n** \n充值68元，即可获得\n\n##[{\"type\":2,\"count\":760},{\"type\":0,\"value\":0,\"count\":30},{\"type\":0,\"value\":552,\"count\":1},{\"type\":0,\"value\":540,\"count\":5},{\"type\":0,\"value\":535,\"count\":3},{\"type\":0,\"value\":536,\"count\":1}]\n\n** \n充值128元，即可获得\n\n##[{\"type\":2,\"count\":1460},{\"type\":0,\"value\":0,\"count\":30},{\"type\":0,\"value\":552,\"count\":1},{\"type\":0,\"value\":530,\"count\":2},{\"type\":0,\"value\":540,\"count\":5},{\"type\":0,\"value\":535,\"count\":5},{\"type\":0,\"value\":536,\"count\":5}]\n\n** \n充值198元，即可获得\n\n##[{\"type\":2,\"count\":2260},{\"type\":0,\"value\":0,\"count\":50},{\"type\":0,\"value\":551,\"count\":1},{\"type\":0,\"value\":530,\"count\":2},{\"type\":0,\"value\":540,\"count\":10},{\"type\":0,\"value\":536,\"count\":10},{\"type\":0,\"value\":537,\"count\":10}]\n\n** \n充值328元，即可获得\n\n##[{\"type\":2,\"count\":3760},{\"type\":0,\"value\":0,\"count\":50},{\"type\":0,\"value\":551,\"count\":1},{\"type\":0,\"value\":552,\"count\":1},{\"type\":0,\"value\":532,\"count\":2},{\"type\":0,\"value\":537,\"count\":15}]\n\n** \n充值648元，即可获得\n\n##[{\"type\":2,\"count\":7480}, {\"type\":0,\"value\":0,\"count\":80},{\"type\":0,\"value\":551,\"count\":1},{\"type\":0,\"value\":552,\"count\":1},{\"type\":0,\"value\":533,\"count\":1},{\"type\":0,\"value\":532,\"count\":2},{\"type\":0,\"value\":537,\"count\":20}]",
+      "mailTitle": "《充值就得奖》活动奖励",
+      "mailBody": "恭喜你完成活动，点击领取活动奖励！",
+      "date": "2014/05/05",
+      "dateDescription": "截止日期2014年5月5日24时",
+    "objective": {
+      "6": {
+        "award": [
+            {"type":0,"value":0,"count":5},
+            {"type":0,"value":540,"count":1}
+        ]
+      },
+      "12": {
+        "award": [
+            {"type":0,"value":0,"count":10},
+            {"type":0,"value":540,"count":2}
+        ]
+      },
+        "30": {
+            "award": [
+                {"type":0,"value":0,"count":20},
+                {"type":0,"value":540,"count":2},
+                {"type":0,"value":534,"count":3}
+            ]
+        },
+        "68": {
+            "award": [
+                {"type":0,"value":0,"count":30},
+                {"type":0,"value":552,"count":1},
+                {"type":0,"value":540,"count":5},
+                {"type":0,"value":535,"count":3},
+                {"type":0,"value":536,"count":1}
+            ]
+        },
+        "128": {
+            "award": [
+                {"type":0,"value":0,"count":30},
+                {"type":0,"value":552,"count":1},
+                {"type":0,"value":530,"count":2},
+                {"type":0,"value":540,"count":5},
+                {"type":0,"value":535,"count":5},
+                {"type":0,"value":536,"count":5}
+            ]
+        },
+        "198": {
+            "award": [
+                {"type":0,"value":0,"count":50},
+                {"type":0,"value":551,"count":1},
+                {"type":0,"value":530,"count":2},
+                {"type":0,"value":540,"count":10},
+                {"type":0,"value":536,"count":10},
+                {"type":0,"value":537,"count":10}
+            ]
+        },
+        "328": {
+            "award": [
+                {"type":0,"value":0,"count":50},
+                {"type":0,"value":551,"count":1},
+                {"type":0,"value":552,"count":1},
+                {"type":0,"value":532,"count":2},
+                {"type":0,"value":537,"count":15}
+            ]
+        },
+        "648": {
+            "award": [
+                {"type":0,"value":0,"count":80},
+                {"type":0,"value":551,"count":1},
+                {"type":0,"value":552,"count":1},
+                {"type":0,"value":533,"count":1},
+                {"type":0,"value":532,"count":2},
+                {"type":0,"value":537,"count":20}
+            ]
+        }
+    }
+  },
+  "LevelUp": {
+    "show": true,
+      "banner":"event-banner-jncj.png",
+    "title": "冲级得礼包",
+    "description": "玩家凡注册之日起一周内冲到6级即可获得丰厚奖励。",
+      "mailTitle": "《冲级得礼包》奖励",
+      "mailBody": "恭喜你完成冲级活动，点击领取奖励！",
+      "date": "2014/05/05",
+      "timeLimit": 604800,
+      "dateDescription": "截止日期为2014年5月5日24时",
+    "level": [
+      {
+        "count": 6,
+        "award": [
+            {"type":1, "count":6000 },
+            { "type":0,"value":0, "count":50 },
+            { "type":0,"value":460, "count":1 },
+            { "type":0,"value":239, "count":1 ,"classLimit":[1]},
+            { "type":0,"value":243, "count":1 ,"classLimit":[2]},
+            { "type":0,"value":247, "count":1 ,"classLimit":[0]},
+            { "type":0,"value":603, "count":1 ,"classLimit":[1]},
+            { "type":0,"value":607, "count":1 ,"classLimit":[2]},
+            { "type":0,"value":599, "count":1 ,"classLimit":[0]},
+            { "type":0,"value":593, "count":1 ,"classLimit":[1]},
+            { "type":0,"value":597, "count":1 ,"classLimit":[2]},
+            { "type":0,"value":589, "count":1 ,"classLimit":[0]},
+            { "type":0,"value":560, "count":40 }
+        ]
+      }
+    ]
+  },
+  "TotalCharge": {
+    "show": false,
+    "title": "累充活动",
+    "description": "累积充值达到指定数值，即可获得相应礼包！",
+      "mailTitle": "VIP等级提升",
+      "mailBody": "VIP等级得到提升，您已经拥有购买相应VIP宝箱的权限以及以礼品。",
+    "level": [
+      {
+        "count": 30,
+        "award": [
+            {"type":2, "count":100 },
+            {"type":0,"value":540,"count":3},
+            {"type":0,"value":539,"count":1}
+        ]
+      },
+        {
+            "count": 100,
+            "award": [
+                {"type":2, "count":100 },
+                {"type":0,"value":534,"count":3},
+                {"type":0,"value":535,"count":2},
+                {"type":0,"value":536,"count":1}
+            ]
+        }
+    ]
+  },
+  "Friend": {
+    "show": true,
+    "title": "朋友去哪儿？！",
+      "banner":"event-banner-zyqne.png",
+    "description": "俗话说“一个好汉三个帮”，身为勇士的你单枪匹马可不成，多加些小伙伴一起冒险吧。活动期间内成功添加20名好友就有惊喜大礼哦！",
+    "mailTitle": "《朋友去哪儿？！》活动奖励",
+    "mailBody": "恭喜你完成《朋友去哪儿？！》活动，点击领取奖励礼包！",
+    "date": "2014/02/28",
+    "dateDescription": "截止日期2014年2月28日24时",
+    "level": [
+      {
+        "count": 20,
+        "award": [
+            { "type":2, "count":50 },
+            { "type":0,"value":0, "count":5 },
+            { "type":0,"value":540, "count":1 }
+        ]
+      }
+    ]
+  },
+  "LoginStreak": {
+    "show": false,
+    "level": [
+      {
+        "count": 1,
+        "award": [ { "type":1, "count":500 } ]
+      },
+      {
+        "count": 2,
+        "award": [ { "type":0, "value":534, "count":2 } ]
+      },
+      {
+        "count": 3,
+        "award": [ { "type":0, "value":540, "count":1 } ]
+      },
+      {
+        "count": 4,
+        "award": [ { "type":0, "value":538, "count":1 } ]
+      },
+      {
+        "count": 5,
+        "award": [
+          { "type":0, "value":534, "count":1 },
+          { "type":0, "value":535, "count":1 },
+          { "type":0, "value":536, "count":1 }
+        ]
+      }
+    ]
+  },
+    "FirstCharge": {
+        "show": true,
+        "title": "首充翻倍大行动",
+        "banner":"event-banner-scfb.png",
+        "description":"首次充值即可获得宝石翻倍的奖励，充多少送多少，妈妈再也不用担心我的宝石了！",
+        "mailTitle": "《首充翻倍大行动》活动奖励",
+        "mailBody": "恭喜你完成活动，点击领取活动奖励！",
+        "date": "2014/05/25",
+        "dateDescription": "截止日期2014年5月25日24时",
+        "objective": {
+            "6": {
+                "award": [
+                    {"type":2,"count":60}
+                ]
+            },
+            "12": {
+                "award": [
+                    {"type":2,"count":130}
+                ]
+            },
+            "30": {
+                "award": [
+                    {"type":2,"count":330}
+                ]
+            },
+            "68": {
+                "award": [
+                    {"type":2,"count":760}
+                ]
+            },
+            "128": {
+                "award": [
+                    {"type":2,"count":1460}
+                ]
+            },
+            "198": {
+                "award": [
+                    {"type":2,"count":2260}
+                ]
+            },
+            "328": {
+                "award": [
+                    {"type":2,"count":3760}
+                ]
+            },
+            "648": {
+                "award": [
+                    {"type":2,"count":7480}
+                ]
+            }
+        }
+    }
+};
