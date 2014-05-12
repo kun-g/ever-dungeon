c50e0a339e8c4fb3398a3ea1cd50955320cfb3cf
Kun.G <nukoug@gmail.com>
2014-05-12 16:55:12 +0800

    Commit changes branch:localWork @ 106 Server:v1.0.7 Table:develop

---
+++ build/costs.js
@@ -0,0 +1,57 @@
+exports.data = [
+    {
+        "costId": 0,
+        "material":[
+            { "type": 1, "count":20},
+            { "type": 0, "value": 0, "count":1},
+            { "type": 0, "value": 1, "count":2},
+            { "type": 0, "value": 2, "count":1},
+            { "type": 0, "value": 3, "count":2},
+            { "type": 0, "value": 4, "count":1},
+            { "type": 0, "value": 5, "count":2}
+        ]
+
+    },
+    {
+        "costId": 1,
+        "material":[
+            { "type": 1, "count":20},
+            { "type": 0, "value": 1, "count":1}
+        ]
+    },
+    {
+        "costId": 2,
+        "material":[
+            { "type": 1, "count":20},
+            { "type": 0, "value": 0, "count":3}
+        ]
+    },
+    {
+        "costId": 3,
+        "material":[
+            { "type": 1, "count":30},
+            { "type": 0, "value": 1, "count":3}
+        ]
+    },
+    {
+        "costId": 4,
+        "material":[
+            { "type": 1, "count":40},
+            { "type": 0, "value": 2, "count":3}
+        ]
+    },
+    {
+        "costId": 5,
+        "material":[
+            { "type": 1, "count":50},
+            { "type": 0, "value": 3, "count":3}
+        ]
+    },
+    {
+        "costId": 6,
+        "material":[
+            { "type": 1, "count":60},
+            { "type": 0, "value": 4, "count":3}
+        ]
+    }
+]
