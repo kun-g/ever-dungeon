a019573cd74fadec4dcf658748b14c2f460f2d25
Kun.G <nukoug@gmail.com>
2014-05-16 13:21:17 +0800

    Commit changes branch:localWork @ 107 Server:v1.0.7 Table:develop

--- build/gmTool.js
+++ build/gmTool.js
@@ -7,8 +7,8 @@ require('./globals');
 //        'Doyle', '豆豆同学丶', '震北冥', '888666', '蛋町' ];
 players = ['jvf'];
 
-//serverName = 'Develop';
-serverName = 'Master';
+serverName = 'Develop';
+//serverName = 'Master';
 
 var config = {
   Develop: {
@@ -80,26 +80,29 @@ initGlobalConfig(null, function () {
   initServer();
   gServerID = -1;
   //dbLib.loadPlayer('Doge', function (err, player) {
-  dbLib.loadPlayer('天走卢克', function (err, player) {
-    function showInventory() {
-      var bag = player.inventory.map(
-                                  function (e, i) { 
-                                    if (!e) return null;
-                                    var ret = { id: e.id, name: e.label, slot: i };
-                                    if (e.enhancement) {
-                                      ret.enhancement = JSON.parse(JSON.stringify(e.enhancement));
-                                    }
-                                    if (player.isEquiped(i)) ret.equip = true;
-                                    return ret;
-                                })
-                                .filter( function (e) { return e; } );
-      logInfo({ diamond: player.diamond, bag: bag});
-    }
-    showInventory();
-    player.migrate();
-    showInventory();
-    player.save();
+  dbClient.keys("*player*", function (err, list) {
+    console.log(list);
   });
+  //dbLib.loadPlayer('天走卢克', function (err, player) {
+  //  function showInventory() {
+  //    var bag = player.inventory.map(
+  //                                function (e, i) { 
+  //                                  if (!e) return null;
+  //                                  var ret = { id: e.id, name: e.label, slot: i };
+  //                                  if (e.enhancement) {
+  //                                    ret.enhancement = JSON.parse(JSON.stringify(e.enhancement));
+  //                                  }
+  //                                  if (player.isEquiped(i)) ret.equip = true;
+  //                                  return ret;
+  //                              })
+  //                              .filter( function (e) { return e; } );
+  //    logInfo({ diamond: player.diamond, bag: bag});
+  //  }
+  //  showInventory();
+  //  player.migrate();
+  //  showInventory();
+  //  player.save();
+  //});
 });
 //async.map(players, function (playerName, cb) {
 //  dbLib.deliverMessage(playerName, rewardMessage, cb);
