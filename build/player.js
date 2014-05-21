54a61cf996b76c1cd09863c7919d8429fb069d0e
Kun.G <nukoug@gmail.com>
2014-05-22 01:06:48 +0800

    Commit changes branch:localWork @ 109 Server:master Table:master

--- build/player.js
+++ build/player.js
@@ -157,39 +157,41 @@
       _ref7 = this.inventory.container;
       for (slot in _ref7) {
         item = _ref7[slot];
-        if (item != null) {
-          if (item.transPrize != null) {
-            flag = true;
-            if (this.isEquiped(slot)) {
-              lv = 0;
-              if (item.enhancement && item.enhancement.length > 0) {
-                lv = item.enhancement.reduce((function(r, i) {
-                  return r + i.level;
-                }), 0);
-              }
-              cfg = require('./transfer').data;
-              if (cfg[item.id]) {
-                p = cfg[item.id].filter((function(_this) {
-                  return function(e) {
-                    return isClassMatch(_this.hero["class"], e.classLimit);
-                  };
-                })(this));
-                item.id = p[0].value;
-                console.log(item.id);
-              }
-              enhanceID = queryTable(TABLE_ITEM, item.id).enhanceID;
-              if ((enhanceID != null) && lv >= 0) {
-                item.enhancement = [
-                  {
-                    id: enhanceID,
-                    level: lv
-                  }
-                ];
-              }
-              continue;
+        if (!(item != null)) {
+          continue;
+        }
+        console.log('X');
+        if (item.transPrize != null) {
+          flag = true;
+          if (this.isEquiped(slot)) {
+            lv = 0;
+            if (item.enhancement && item.enhancement.length > 0) {
+              lv = item.enhancement.reduce((function(r, i) {
+                return r + i.level;
+              }), 0);
             }
-            this.sellItem(slot);
+            cfg = require('./transfer').data;
+            if (cfg[item.id]) {
+              p = cfg[item.id].filter((function(_this) {
+                return function(e) {
+                  return isClassMatch(_this.hero["class"], e.classLimit);
+                };
+              })(this));
+              item.id = p[0].value;
+              console.log(item.id);
+            }
+            enhanceID = queryTable(TABLE_ITEM, item.id).enhanceID;
+            if ((enhanceID != null) && lv >= 0) {
+              item.enhancement = [
+                {
+                  id: enhanceID,
+                  level: lv
+                }
+              ];
+            }
+            continue;
           }
+          this.sellItem(slot);
         }
       }
       prize = queryTable(TABLE_CONFIG, 'InitialEquipment');
@@ -197,6 +199,7 @@
         if (!(this.equipment[slot] == null)) {
           continue;
         }
+        console.log('Y');
         flag = true;
         this.claimPrize(prize[slot].filter((function(_this) {
           return function(e) {
