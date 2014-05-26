7eea876849ff852f8daaebcf15f208e711596e66
Kun.G <nukoug@gmail.com>
2014-05-26 18:06:03 +0800

    Commit changes branch:localWork @ 110 Server:v1.0.8 Table:develop

--- build/player.js
+++ build/player.js
@@ -272,8 +272,8 @@
         dis = diffDate(this.loginStreak.date);
         if (dis === 0) {
           flag = false;
-        } else if (dis > 1) {
-          this.loginStreak.count = 0;
+        } else {
+          this.loginStreak.count += 1;
         }
       } else {
         this.loginStreak.count = 0;
@@ -318,7 +318,6 @@
           return !e.vip || _this.vipLevel() > e.vip;
         };
       })(this)));
-      this.loginStreak.count += 1;
       if (this.loginStreak.count >= queryTable(TABLE_DP).length) {
         this.loginStreak.count = 0;
       }
