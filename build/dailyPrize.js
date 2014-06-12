e49b47809195ce81eddf8945a2e7362581ee4b3f
Kun.G <nukoug@gmail.com>
2014-06-12 10:24:56 +0800

    123

---
+++ build/dailyPrize.js
@@ -0,0 +1,374 @@
+exports.data = [
+    {
+        "prizeId": 0,
+        "prize": [
+            {
+                "type": 0,
+                "value":538,
+                "count":1
+            }
+        ]
+    },
+    {
+        "prizeId":1,
+        "prize": [
+            {
+                "type": 0,
+                "value":853,
+                "count":6
+            },
+            {
+                "type": 0,
+                "value":853,
+                "count":6,
+                "vip":1
+            }
+        ]
+    },
+    {
+        "prizeId":2,
+        "prize": [
+            {
+                "type": 0,
+                "value":540,
+                "count":1
+            }
+        ]
+    },
+    {
+        "prizeId":3,
+        "prize": [
+            {
+                "type": 0,
+                "value":858,
+                "count":3
+            },
+            {
+                "type": 0,
+                "value":858,
+                "count":3,
+                "vip":1
+            }
+        ]
+    },
+    {
+        "prizeId":4,
+        "prize": [
+            {
+                "type": 2,
+                "count":20
+            }
+        ]
+    },
+    {
+        "prizeId":5,
+        "prize": [
+            {
+                "type": 0,
+                "value":538,
+                "count":1
+            }
+        ]
+    },
+    {
+        "prizeId":6,
+        "prize": [
+            {
+                "type": 0,
+                "value":854,
+                "count":10
+            },
+            {
+                "type": 0,
+                "value":854,
+                "count":10,
+                "vip":1
+            }
+        ]
+    },
+    {
+        "prizeId":7,
+        "prize": [
+            {
+                "type": 0,
+                "value":540,
+                "count":1
+            }
+        ]
+    },
+    {
+        "prizeId":8,
+        "prize": [
+            {
+                "type": 0,
+                "value":858,
+                "count":8
+            },
+            {
+                "type": 0,
+                "value":858,
+                "count":8,
+                "vip":1
+            }
+        ]
+    },
+    {
+        "prizeId":9,
+        "prize": [
+            {
+                "type": 2,
+                "count":30
+            }
+        ]
+    },
+    {
+        "prizeId":10,
+        "prize": [
+            {
+                "type": 0,
+                "value":538,
+                "count":2
+            }
+        ]
+    },
+    {
+        "prizeId":11,
+        "prize": [
+            {
+                "type": 0,
+                "value":855,
+                "count":6
+            },
+            {
+                "type": 0,
+                "value":855,
+                "count":6,
+                "vip":1
+            }
+        ]
+    },
+    {
+        "prizeId":12,
+        "prize": [
+            {
+                "type": 0,
+                "value":540,
+                "count":2
+            }
+        ]
+    },
+    {
+        "prizeId":13,
+        "prize": [
+            {
+                "type": 0,
+                "value":859,
+                "count":4
+            },
+            {
+                "type": 0,
+                "value":859,
+                "count":4,
+                "vip":1
+            }
+        ]
+    },
+    {
+        "prizeId":14,
+        "prize": [
+            {
+                "type": 2,
+                "count":40
+            }
+        ]
+    },
+    {
+        "prizeId":15,
+        "prize": [
+            {
+                "type": 0,
+                "value":538,
+                "count":2
+            }
+        ]
+    },
+    {
+        "prizeId":16,
+        "prize": [
+            {
+                "type": 0,
+                "value":855,
+                "count":10
+            },
+            {
+                "type": 0,
+                "value":855,
+                "count":10,
+                "vip":2
+            }
+        ]
+    },
+    {
+        "prizeId":17,
+        "prize": [
+            {
+                "type": 0,
+                "value":540,
+                "count":2
+            }
+        ]
+    },
+    {
+        "prizeId":18,
+        "prize": [
+            {
+                "type": 0,
+                "value":859,
+                "count":8
+            },
+            {
+                "type": 0,
+                "value":859,
+                "count":8,
+                "vip":2
+            }
+        ]
+    },
+    {
+        "prizeId":19,
+        "prize": [
+            {
+                "type": 2,
+                "count":50
+            }
+        ]
+    },
+    {
+        "prizeId":20,
+        "prize": [
+            {
+                "type": 0,
+                "value":538,
+                "count":3
+            }
+        ]
+    },
+    {
+        "prizeId":21,
+        "prize": [
+            {
+                "type": 0,
+                "value":856,
+                "count":6
+            },
+            {
+                "type": 0,
+                "value":856,
+                "count":6,
+                "vip":2
+            }
+        ]
+    },
+    {
+        "prizeId":22,
+        "prize": [
+            {
+                "type": 0,
+                "value":540,
+                "count":3
+            }
+        ]
+    },
+    {
+        "prizeId":23,
+        "prize": [
+            {
+                "type": 0,
+                "value":860,
+                "count":5
+            },
+            {
+                "type": 0,
+                "value":860,
+                "count":5,
+                "vip":2
+            }
+        ]
+    },
+    {
+        "prizeId":24,
+        "prize": [
+            {
+                "type": 2,
+                "count":60
+            }
+        ]
+    },
+    {
+        "prizeId":25,
+        "prize": [
+            {
+                "type": 0,
+                "value":539,
+                "count":2
+            }
+        ]
+    },
+    {
+        "prizeId":26,
+        "prize": [
+            {
+                "type": 0,
+                "value":857,
+                "count":6
+            },
+            {
+                "type": 0,
+                "value":857,
+                "count":6,
+                "vip":2
+            }
+        ]
+    },
+    {
+        "prizeId":27,
+        "prize": [
+            {
+                "type": 2,
+                "count":20
+            }
+        ]
+    },
+    {
+        "prizeId":28,
+        "prize": [
+            {
+                "type": 2,
+                "count":150
+            },
+            {
+                "type": 2,
+                "count":150,
+                "vip":2
+            }
+        ]
+    },
+    {
+        "prizeId":29,
+        "prize": [
+            {
+                "type": 2,
+                "count":200
+            }
+        ]
+    },
+    {
+        "prizeId":30,
+        "prize": [
+            {
+                "type": 2,
+                "count":400
+            }
+        ]
+    }
+]
