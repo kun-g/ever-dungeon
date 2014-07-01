ea34282df2cf305905dcaa3912da81fb077dfdc9
faruba <farubafaruba@126.com>
2014-07-01 14:42:25 +0800

    Commit changes branch:localWork @ 126 Server:master Table:master

--- build/dungeon.js
+++ build/dungeon.js
@@ -203,7 +203,7 @@
       return -1;
     };
     placeUnit = function(lRule, lConfig, single) {
-      var count, idList, proList, result, _k, _len2, _ref6, _ref7;
+      var count, idList, result, _k, _len2, _ref6;
       result = [];
       for (_k = 0, _len2 = lRule.length; _k < _len2; _k++) {
         r = lRule[_k];
@@ -226,17 +226,12 @@
         if (r.pool != null) {
           idList = selectFromPool(r.pool, count);
           count = 1;
-          proList = (_ref7 = rules.pool[r.pool].property) != null ? _ref7 : [];
         }
         idList.forEach(function(c) {
           var k, u, v;
           u = {};
           for (k in c) {
             v = c[k];
-            u[k] = v;
-          }
-          for (k in proList) {
-            v = proList[k];
             u[k] = v;
           }
           u.count = count;
@@ -290,7 +285,6 @@
         i++;
       }
     }
-    console.log('the ----res', result);
     return result;
   };
 
