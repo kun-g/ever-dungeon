167159fe91cdf26b2015eb476cb1478a18f94016
faruba <farubafaruba@126.com>
2014-07-02 16:22:20 +0800

    Commit changes branch:localWork @ 126 Server:master Table:master

--- build/serializer.js
+++ build/serializer.js
@@ -119,7 +119,7 @@
     Serializer.prototype.restore = function(data) {
       var k, v;
       console.log('debug restore-----------', data, '----');
-      ShowMeTheStack();
+      showMeTheStack();
       if (data == null) {
         return this;
       }
