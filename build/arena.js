bad08a852d4ef78ef2c6e66b8d777ab635a3dcc7
faruba <farubafaruba@126.com>
2014-07-03 15:02:28 +0800

    Commit changes branch:localWork @ 126 Server:master Table:master

--- build/arena.js
+++ build/arena.js
@@ -2,49 +2,49 @@ exports.data = [
     {
         "top":5,
         "prize":[
-            {"type":0, "count":5000}
+            {"type":1, "count":5000}
         ]
     },
     {
         "top":10,
         "prize":[
-            {"type":0, "count":3000}
+            {"type":1, "count":3000}
         ]
     },
     {
         "top":20,
         "prize":[
-            {"type":0, "count":2000}
+            {"type":1, "count":2000}
         ]
     },
     {
         "top":50,
         "prize":[
-            {"type":0, "count":1000}
+            {"type":1, "count":1000}
         ]
     },
     {
         "top":100,
         "prize":[
-            {"type":0, "count":500}
+            {"type":1, "count":500}
         ]
     },
     {
         "top":300,
         "prize":[
-            {"type":0, "count":250}
+            {"type":1, "count":250}
         ]
     },
     {
         "top":500,
         "prize":[
-            {"type":0, "count":150}
+            {"type":1, "count":150}
         ]
     },
     {
         "top":1000,
         "prize":[
-            {"type":0, "count":100}
+            {"type":1, "count":100}
         ]
     }
 
