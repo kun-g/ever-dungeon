c01b458d0f16edb26b0c7208e7c38ce7137e69e4
faruba <farubafaruba@126.com>
2014-07-11 21:47:14 +0800

    Commit changes branch:localWork @ 129 Server:buff Table:master

--- build/dbWrapper.js
+++ build/dbWrapper.js
@@ -193,7 +193,7 @@
         if (heroNames) {
           return async.eachSeries(heroNames, function(e, cb) {
             return getPlayerHero(e, wrapCallback(this, function(err, heroData) {
-              heros.push(heroData);
+              heros = heros.concat(heroData);
               return cb();
             }));
           }, function() {
