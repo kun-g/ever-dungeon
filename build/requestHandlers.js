0cb374b4a58abaeb645dff18f1d67874ba92ecba
faruba <farubafaruba@126.com>
2014-08-13 18:25:50 +0800

    Commit changes branch:develop @ 147 Server:userBind Table:develop

--- build/requestHandlers.js
+++ build/requestHandlers.js
@@ -1,5 +1,5 @@
 (function() {
-  var DBWrapper, Player, async, dbLib, getPlayerHero, helperLib, http, https, loadPlayer, loginBy, moment, wrapReceipt, _ref;
+  var DBWrapper, Player, addMercenaryMember, async, dbLib, getPlayerHero, helperLib, http, https, loadPlayer, loginBy, moment, updateMercenaryMember, wrapReceipt, _ref;
 
   require('./define');
 
@@ -7,7 +7,7 @@
 
   helperLib = require('./helper');
 
-  _ref = require('./dbWrapper'), DBWrapper = _ref.DBWrapper, getPlayerHero = _ref.getPlayerHero;
+  _ref = require('./dbWrapper'), DBWrapper = _ref.DBWrapper, updateMercenaryMember = _ref.updateMercenaryMember, addMercenaryMember = _ref.addMercenaryMember, getPlayerHero = _ref.getPlayerHero;
 
   async = require('async');
 
@@ -20,39 +20,10 @@
   Player = require('./player').Player;
 
   loginBy = function(arg, token, callback) {
-    var AppSecret, appID, appKey, options, passport, passportType, path, req, sign;
+    var appID, appKey, options, passport, passportType, path, req, sign;
     passportType = arg.tp;
     passport = arg.id;
     switch (passportType) {
-      case LOGIN_ACCOUNT_TYPE_DK_Android:
-        appID = '3319334';
-        appKey = 'kavpXwRFFa4rjcUy1idmAkph';
-        AppSecret = 'KvCbUBBpAUvkKkC9844QEb8CB7pHnl5v';
-        sign = md5Hash(appID + appKey + passport + token + AppSecret);
-        path = 'http://sdk.m.duoku.com/openapi/sdk/checksession?appid=' + appID + '&appkey=' + appKey + '&uid=' + passport + '&sessionid=' + token + '&clientsecret=' + sign;
-        return http.get(path, function(res) {
-          res.setEncoding('utf8');
-          return res.on('data', function(chunk) {
-            var result;
-            result = JSON.parse(chunk);
-            logInfo({
-              action: 'login',
-              type: passportType,
-              code: result
-            });
-            if (result.error_code === '0') {
-              return callback(null);
-            } else {
-              return callback(Error(RET_LoginFailed));
-            }
-          });
-        }).on('error', function(e) {
-          return logError({
-            action: 'login',
-            type: LOGIN_ACCOUNT_TYPE_DK,
-            error: e
-          });
-        });
       case LOGIN_ACCOUNT_TYPE_91_Android:
       case LOGIN_ACCOUNT_TYPE_91_iOS:
         switch (passportType) {
@@ -213,12 +184,7 @@
               return loginBy(arg, arg.tk, cb);
             }
           }, function(cb) {
-            var tp;
-            tp = arg.tp;
-            if (arg.atp != null) {
-              tp = arg.atp;
-            }
-            return loadPlayer(tp, arg.id, cb);
+            return loadPlayer(arg.tp, arg.id, cb);
           }, function(player, cb) {
             var ev, msg, time;
             if (player) {
@@ -706,20 +672,8 @@
     RPC_BindSubAuth: {
       id: 105,
       func: function(arg, player, handler, rpcID, socket) {
-        var account;
-        account = -1;
-        if (player) {
-          account = player.accountID;
-        }
-        return dbLib.bindAuth(account, arg.typ, arg.id, arg.pass, function(err, account) {
-          if (account === -1 || account === '-1') {
-            return handler([
-              {
-                REQ: rpcID,
-                RET: RET_AccountHaveNoHero
-              }
-            ]);
-          } else {
+        if (player != null) {
+          return dbLib.bindAuth(player.accountID, arg.typ, arg.id, arg.pass, function(err, account) {
             return handler([
               {
                 REQ: rpcID,
@@ -727,8 +681,15 @@
                 aid: account
               }
             ]);
-          }
-        });
+          });
+        } else {
+          return handler([
+            {
+              REQ: rpcID,
+              RET: RET_AccountHaveNoHero
+            }
+          ]);
+        }
       },
       args: {}
     },
