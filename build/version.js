bca4d09425bef9b4a8dd923d4e9d232e2e082144
faruba <farubafaruba@126.com>
2014-08-21 15:11:03 +0800

    Commit changes branch:develop @ 149 Server:master Table:develop

--- build/version.js
+++ build/version.js
@@ -2,7 +2,7 @@ exports.data = {
           "resource_version": 149,
           "quest": "0.0.4",
           "stage": "0.1.5",
-          "bin_version": "1.0.8",
+          "bin_version": "1.0.9",
           "bin_version_need": "1.0.6",
           "url": "http://hotupdate.qiniudn.com",
           "bin_url": "http://fir.im/rX4"
