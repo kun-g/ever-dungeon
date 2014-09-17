28e29112d10492f3be107e7943845fce24d5258b
Kun <nukoug@gmail.com>
2014-09-17 18:32:23 +0800

    Commit changes branch:localWork @ 129 Server:feature/cluster Table:hotfix

--- build/gate.js
+++ build/gate.js
@@ -145,7 +145,7 @@
     gServerID = queryTable(TABLE_CONFIG, 'ServerID');
     gServerConfig = queryTable(TABLE_CONFIG, 'ServerConfig')[gServerID];
     backendManager.init(gServerConfig.Gate);
-    return startSocketIOServer(backendManager, 7757);
+    return startTcpServer(backendManager, 7757);
   });
 
 }).call(this);
