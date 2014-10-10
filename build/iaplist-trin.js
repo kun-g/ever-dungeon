d17ffd9662fae3ebd637774f128b6d4ef61b8161
Jovismac <ijovi23@gmail.com>
2014-10-10 22:52:25 +0800

    no message

---
+++ build/iaplist-trin.js
@@ -0,0 +1,43 @@
+exports.data = {
+    "channel": "Default-CH",
+    "currency": "CNY",
+    "symbol": "￥",
+    "list":[
+        {
+            "price": 6,
+            "gem": 60
+        },
+        {
+            "price": 12,
+            "gem": 130
+        },
+        {
+            "price": 30,
+            "gem": 330
+        },
+        {
+            "price": 68,
+            "gem": 760
+        },
+        {
+            "price": 128,
+            "gem": 1460
+        },
+        {
+            "price": 198,
+            "gem": 328
+        },
+        {
+            "price": 328,
+            "gem": 3760
+        },
+        {
+            "price": 648,
+            "gem": 7480
+        },
+        {
+            "price": 25,
+            "gem": 2500
+        }
+    ]
+};
