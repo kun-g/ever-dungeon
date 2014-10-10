27542e51a83d5f1cc6f5c79a339d1805f8f96906
Jovismac <ijovi23@gmail.com>
2014-10-10 22:59:53 +0800

    Commit changes branch:develop @ 153 Server:master Table:master

--- build/serverlist-appstore.js
+++
@@ -1,6 +0,0 @@
-exports.data = [
-    {
-        "name": "master",
-        "address":{ip:"60.191.205.25", port:7757}
-    }
-];
--- build/serverlist-teebik.js
+++
@@ -1,6 +0,0 @@
-exports.data = [
-    {
-        "name": "master",
-        "address":{ip:"148.251.128.11", port:7757}
-    }
-];
