d8208617eca01e502d7da354f38242d4186562b1
Jovismac <ijovi23@gmail.com>
2014-10-15 20:39:18 +0800

    update applyMultiVersion.sh

--- applyMultiVersion.sh
+++ applyMultiVersion.sh
@@ -2,29 +2,29 @@
 Version=$1
 SEARCH_DIR=($@)
 
-#if file name with -trin ,then it must be mulity version file.
-#but if can't find file's name with -xxx. use xxx-trin as default
+#if file name with -multi ,then it must be multiple version file.
+#but if can't find file's name includes -multy-xxx, use -multi as default
 
 
 for ((i=1; i <${#SEARCH_DIR[@]}; i++))
 do
   dir=${SEARCH_DIR[$i]}
-  mulityVersionFileList=`(ls $dir/*-trin.*)`
+  multiVersionFileList=`(ls $dir/*-multi.*)`
 
-  for fileWithPath in $mulityVersionFileList
+  for fileWithPath in $multiVersionFileList
   do
-    targetFile=`(echo $fileWithPath | sed -e 's/-trin//g')`
-    removeFils=`(echo $fileWithPath | sed -e 's/-trin/-*/g')`
-    wantFile=`(echo $fileWithPath | sed -e 's/-trin/-'$1'/g')`
-    if [ -e $wantFile ]
+    targetFile=`(echo $fileWithPath | $SED -e 's/-multi//g')`
+    removeFiles=`(echo $fileWithPath | $SED -e 's/-multi/-multi-*/g')`
+    wantedFile=`(echo $fileWithPath | $SED -e 's/-multi/-multi-'$1'/g')`
+    if [ -e $wantedFile ]
     then
-      sourceFile=$wantFile
+      sourceFile=$wantedFile
     else
       sourceFile=$fileWithPath
     fi
 
-    cp $sourceFile $targetFile 
-    rm $removeFils 
+    cp -f $sourceFile $targetFile 
+    rm -f $removeFiles 
   done
 done
 
