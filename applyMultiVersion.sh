4d21459b12a1c088359bb44f39f74b714b4de96d
faruba <farubafaruba@126.com>
2014-10-17 15:03:27 +0800

    remove debug msg

--- applyMultiVersion.sh
+++ applyMultiVersion.sh
@@ -5,8 +5,6 @@ SEARCH_DIR=($@)
 #if file name with -multi ,then it must be multiple version file.
 #but if can't find file's name includes -multy-xxx, use -multi as default
 
-echo $SED
-
 for ((i=1; i <${#SEARCH_DIR[@]}; i++))
 do
   dir=${SEARCH_DIR[$i]}
@@ -17,7 +15,6 @@ do
     targetFile=`(echo $fileWithPath | $SED -e 's/-multi//g')`
     removeFiles=`(echo $fileWithPath | $SED -e 's/-multi/-multi*/g')`
     wantedFile=`(echo $fileWithPath | $SED -e 's/-multi/-multi-'$1'/g')`
-		echo $wantedFile "00000"
     if [ -e $wantedFile ]
     then
       sourceFile=$wantedFile
