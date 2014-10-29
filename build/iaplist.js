7db8d68dbb16224bb34f8dbc8fdb8b1eea9882dd
faruba <farubafaruba@126.com>
2014-10-29 11:12:18 +0800

    Commit changes branch:develop @ 153 Server:develop Table:master

--- build/iaplist.js
+++ build/iaplist.js
@@ -37,8 +37,8 @@ exports.data = {
         },
         {
             "price": 3.99,
-            "gem": 2500,
-            "enable": false
+            "gem": 0,
+            "enable": true
         }
     ]
 };
