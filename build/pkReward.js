0e95a3cdeb27ec3fb6c54395c91dba960e9bb7ae
faruba <farubafaruba@126.com>
2014-12-15 14:43:59 +0800

    add pkReward.js

---
+++ build/pkReward.js
@@ -0,0 +1,29 @@
+exports.data = [
+    {
+        top:5,
+        prize: [
+            {"type":1, "count":150}
+        ]
+    },
+    {
+        top:15,
+        prize: [
+            {"type":1, "count":50}
+        ]
+    },
+    {
+        top:55,
+        prize: [
+            {"type":1, "count":50}
+        ]
+    },
+	{
+		top: Infinity,
+		prize: [
+            {"type":1, "count":50}
+        ]
+	},
+];
+
+
+
