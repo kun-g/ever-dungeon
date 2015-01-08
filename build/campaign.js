3fe57eacc6ca7172721aa56d9736f3d460518c87
faruba <farubafaruba@126.com>
2015-01-08 19:55:19 +0800

    v.1.1.28 appstore kunG some codes

--- build/campaign.js
+++ build/campaign.js
@@ -1,282 +1,172 @@
-iaplist = require('./iaplist').data.list;
-exports.data = {
-    "Charge": {
-        "show": true,
-        "title": "中秋充值奖励",
-        "banner":"event-banner-wzydr.png",
-        "description": [
-            "单笔充值达到6元，12元，30元，68元，128元，198元，328元，648元分别得到奖励。",
-            "***奖励内容：",
-            "** \n充值6元，即可获得",
-            "##[{\"type\":2,\"count\":60},{\"type\":0,\"value\":853,\"count\":5},{\"type\":0,\"value\":540,\"count\":2}]",
-            "** \n充值12元，即可获得",
-            "##[{\"type\":2,\"count\":130},{\"type\":0,\"value\":853,\"count\":10},{\"type\":0,\"value\":540,\"count\":2},{\"type\":0,\"value\":538,\"count\":1}]",
-            "** \n充值30元，即可获得",
-            "##[{\"type\":2,\"count\":330},{\"type\":0,\"value\":871,\"count\":5},{\"type\":0,\"value\":540,\"count\":3},{\"type\":0,\"value\":538,\"count\":1}]",
-            "** \n充值68元，即可获得",
-            "##[{\"type\":2,\"count\":760},{\"type\":0,\"value\":871,\"count\":5},{\"type\":0,\"value\":538,\"count\":1},{\"type\":0,\"value\":540,\"count\":3},{\"type\":0,\"value\":860,\"count\":1}]",
-            "** \n充值128元，即可获得",
-            "##[{\"type\":2,\"count\":1460},{\"type\":0,\"value\":871,\"count\":10},{\"type\":0,\"value\":539,\"count\":1},{\"type\":0,\"value\":860,\"count\":3},{\"type\":0,\"value\":552,\"count\":1}]",
-            "** \n充值198元，即可获得",
-            "##[{\"type\":2,\"count\":2260},{\"type\":0,\"value\":871,\"count\":20},{\"type\":0,\"value\":539,\"count\":1},{\"type\":0,\"value\":552,\"count\":1},{\"type\":0,\"value\":860,\"count\":6}]",
-            "** \n充值328元，即可获得",
-            "##[{\"type\":2,\"count\":3760},{\"type\":0,\"value\":871,\"count\":20},{\"type\":0,\"value\":28,\"count\":4},{\"type\":0,\"value\":551,\"count\":1},{\"type\":0,\"value\":860,\"count\":10}]",
-            "** \n充值648元，即可获得",
-            "##[{\"type\":2,\"count\":7480}, {\"type\":0,\"value\":871,\"count\":30},{\"type\":0,\"value\":28,\"count\":10},{\"type\":0,\"value\":552,\"count\":1},{\"type\":0,\"value\":551,\"count\":1},{\"type\":0,\"value\":860,\"count\":20}]"
-        ],
-        "mailTitle": "《中秋充值》活动奖励",
-        "mailBody": "恭喜你完成活动，点击领取活动奖励！",
-        "date": "2014/09/15",
-        "dateDescription": "截止日期2014年9月15日24时",
-        "objective": {
-            "6": {
-                "award": [
-                    {"type":0,"value":853,"count":5},
-                    {"type":0,"value":540,"count":2}
-                ]
-            },
-            "12": {
-                "award": [
-                    {"type":0,"value":853,"count":10},
-                    {"type":0,"value":540,"count":2},
-                    {"type":0,"value":538,"count":1}
-                ]
-            },
-            "30": {
-                "award": [
-                    {"type":0,"value":871,"count":5},
-                    {"type":0,"value":540,"count":3},
-                    {"type":0,"value":538,"count":1}
-                ]
-            },
-            "68": {
-                "award": [
-                    {"type":0,"value":871,"count":5},
-                    {"type":0,"value":538,"count":1},
-                    {"type":0,"value":540,"count":4},
-                    {"type":0,"value":860,"count":1}
-                ]
-            },
-            "128": {
-                "award": [
-                    {"type":0,"value":871,"count":10},
-                    {"type":0,"value":539,"count":1},
-                    {"type":0,"value":860,"count":3},
-                    {"type":0,"value":552,"count":1}
-                ]
-            },
-            "198": {
-                "award": [
-                    {"type":0,"value":871,"count":20},
-                    {"type":0,"value":539,"count":1},
-                    {"type":0,"value":552,"count":1},
-                    {"type":0,"value":860,"count":6}
-                ]
-            },
-            "328": {
-                "award": [
-                    {"type":0,"value":871,"count":20},
-                    {"type":0,"value":28,"count":4},
-                    {"type":0,"value":551,"count":1},
-                    {"type":0,"value":860,"count":10}
-                ]
-            },
-            "648": {
-                "award": [
-                    {"type":0,"value":871,"count":30},
-                    {"type":0,"value":28,"count":10},
-                    {"type":0,"value":552,"count":1},
-                    {"type":0,"value":551,"count":1},
-                    {"type":0,"value":860,"count":20}
-                ]
-            }
-        }
-    },
-    "DuanwuCharge": {
-        "show": true,
-        "title": "端午粽子派送",
-        "banner":"event-banner-dwj.png",
-        "description": [
-            "单笔充值达到6元，12元，30元，68元，128元，198元，328元，648元分别得到奖励。",
-            "***奖励内容：",
-            "充值6元，即可获得",
-            "##[{\"type\":2,\"count\":60},{\"type\":0,\"value\":862,\"count\":1}]",
-            "** \n充值12元，即可获得",
-            "##[{\"type\":2,\"count\":130},{\"type\":0,\"value\":862,\"count\":3}]",
-            "** \n充值30元，即可获得",
-            "##[{\"type\":2,\"count\":330},{\"type\":0,\"value\":862,\"count\":7}]",
-            "** \n充值68元，即可获得",
-            "##[{\"type\":2,\"count\":760},{\"type\":0,\"value\":862,\"count\":10}]",
-            "** \n充值128元，即可获得",
-            "##[{\"type\":2,\"count\":1460},{\"type\":0,\"value\":862,\"count\":10},{\"type\":0,\"value\":863,\"count\":3}]",
-            "** \n充值198元，即可获得",
-            "##[{\"type\":2,\"count\":2260},{\"type\":0,\"value\":862,\"count\":12},{\"type\":0,\"value\":863,\"count\":6}]",
-            "** \n充值328元，即可获得",
-            "##[{\"type\":2,\"count\":3760},{\"type\":0,\"value\":862,\"count\":15},{\"type\":0,\"value\":863,\"count\":9}]",
-            "** \n充值648元，即可获得",
-            "##[{\"type\":2,\"count\":7480}, {\"type\":0,\"value\":862,\"count\":17},{\"type\":0,\"value\":863,\"count\":11}]"
-        ],
-        "mailTitle": "《端午送粽子》活动奖励",
-        "mailBody": "恭喜你完成活动，点击领取礼包！",
-        "date": "2014/06/05",
-        "dateDescription": "截止日期2014年6月5日24时",
-        "objective": {
-            "6": {
-                "award": [
-                    {"type":0,"value":862,"count":1}
-                ]
-            },
-            "12": {
-                "award": [
-                    {"type":0,"value":862,"count":3}
-                ]
-            },
-            "30": {
-                "award": [
-                    {"type":0,"value":862,"count":7}
-                ]
-            },
-            "68": {
-                "award": [
-                    {"type":0,"value":862,"count":10}
-                ]
-            },
-            "128": {
-                "award": [
-                    {"type":0,"value":862,"count":10},
-                    {"type":0,"value":863,"count":3}
-                ]
-            },
-            "198": {
-                "award": [
-                    {"type":0,"value":862,"count":12},
-                    {"type":0,"value":863,"count":6}
-                ]
-            },
-            "328": {
-                "award": [
-                    {"type":0,"value":862,"count":15},
-                    {"type":0,"value":863,"count":9}
-                ]
-            },
-            "648": {
-                "award": [
-                    {"type":0,"value":862,"count":17},
-                    {"type":0,"value":863,"count":11}
-                ]
-            }
-        }
-    },
-  "LevelUp": {
-    "show": true,
-      "banner":"event-banner-jncj.png",
-    "title": "冲级得礼包",
-    "description": ["玩家凡注册之日起一周内冲到6级即可获得丰厚奖励。"],
-      "mailTitle": "《冲级得礼包》奖励",
-      "mailBody": "恭喜你完成冲级活动，点击领取奖励！",
-      "date": "2014/07/05",
-      "timeLimit": 604800,
-      "dateDescription": "截止日期为2014年7月5日24时",
-    "level": [
-      {
-        "count": 6,
-        "award": [
-            {"type":1, "count":6000 },
-            { "type":2,"count":150 }
-        ]
-      }
-    ]
-  },
-  "TotalCharge": {
-    "show": false,
-    "title": "累充活动",
-    "description": ["累积充值达到指定数值，即可获得相应礼包！"],
-      "mailTitle": "VIP等级提升",
-      "mailBody": "VIP等级得到提升，您已经拥有购买相应VIP宝箱的权限以及以礼品。",
-    "level": [
-      {
-        "count": 30,
-        "award": [
-            {"type":2, "count":100 },
-            {"type":0,"value":540,"count":3},
-            {"type":0,"value":539,"count":1}
-        ]
-      },
-        {
-            "count": 100,
-            "award": [
-                {"type":2, "count":300 },
-                {"type":0,"value":540,"count":3},
-                {"type":0,"value":539,"count":1}
-            ]
-        },
-        {
-            "count": 150,
-            "award": [
-                {"type":0,"value":540,"count":3},
-                {"type":0,"value":539,"count":1},
-                {"type":0,"value":871,"count":10}
-            ]
-        },
-        {
-            "count": 250,
-            "award": [
-                {"type":0,"value":540,"count":3},
-                {"type":0,"value":539,"count":1},
-                {"type":0,"value":871,"count":20}
-            ]
-        },
-        {
-            "count": 400,
-            "award": [
-                {"type":0,"value":540,"count":3},
-                {"type":0,"value":871,"count":30}
-            ]
-        },
-        {
-            "count": 850,
-            "award": [
-                {"type":0,"value":540,"count":3}
-            ]
-        },
-        {
-            "count": 1400,
-            "award": [
-                {"type":0,"value":540,"count":3}
-            ]
-        }
-    ]
-  },
-  "Friend": {
-    "show": true,
-    "title": "朋友去哪儿？！",
-      "banner":"event-banner-zyqne.png",
-    "description": ["俗话说“一个好汉三个帮”，身为勇士的你单枪匹马可不成，多加些小伙伴一起冒险吧。活动期间内成功添加20名好友就有惊喜大礼哦！"],
-    "mailTitle": "《朋友去哪儿？！》活动奖励",
-    "mailBody": "恭喜你完成《朋友去哪儿？！》活动，点击领取奖励礼包！",
-    "date": "2014/02/28",
-    "dateDescription": "截止日期2014年2月28日24时",
-    "level": [
-      {
-        "count": 20,
-        "award": [
-            { "type":2, "count":50 },
-            { "type":0,"value":0, "count":5 },
-            { "type":0,"value":540, "count":1 }
-        ]
-      }
-    ]
-  },
-    "FirstCharge": {
-        "show": true,
-        "title": "首充翻倍大行动",
-        "banner":"event-banner-scfb.png",
-        "description":["首次充值即可获得宝石翻倍的奖励，充多少送多少，妈妈再也不用担心我的宝石了！"],
-        "mailTitle": "《首充翻倍大行动》活动奖励",
-        "mailBody": "恭喜你完成活动，点击领取活动奖励！",
-        "date": "2016/12/25",
-        "dateDescription": "截止日期2016年12月25日24时",
-        // iaplist.list will modify by initCampaignTable =>{award:[{type:2,count:60}}
-        "objective" : iaplist
+(function() {
+  "use strict";
+  var Campaign, libCounter, libTime, utils;
+
+  libTime = require('./timeUtils.js');
+
+  libCounter = require('./counter.js');
+
+  utils = {
+    libTime: libTime,
+    libCounter: libCounter
+  };
+
+  Campaign = (function() {
+    function Campaign(config) {
+      this.config = config;
     }
-};
+
+    Campaign.prototype.getLogicOperator = function(obj) {
+      var key, keys, _i, _len;
+      keys = ['or', 'and', 'not'];
+      for (_i = 0, _len = keys.length; _i < _len; _i++) {
+        key = keys[_i];
+        if (obj[key]) {
+          return key;
+        }
+      }
+      return null;
+    };
+
+    Campaign.prototype.conditionCheck = function(condition, object, time) {
+      var flag;
+      if (!condition) {
+        return true;
+      }
+      flag = true;
+      if (Array.isArray(condition)) {
+        flag = condition.reduce((function(_this) {
+          return function(r, e) {
+            var thisData;
+            if (!r) {
+              return r;
+            }
+            switch (e.type) {
+              case 'counter':
+                return _this.getInfo(object).counter[e.func](time);
+              case 'time':
+                thisData = {
+                  Timestamp: object.timestamp
+                };
+                return libTime.verify(time, e.timeExpr, thisData);
+              case 'function':
+                thisData = {
+                  object: object,
+                  time: time
+                };
+                return e.func(thisData, utils);
+            }
+          };
+        })(this), true);
+      } else {
+        switch (this.getLogicOperator(condition)) {
+          case 'or':
+            return condition.or.reduce((function(_this) {
+              return function(r, e) {
+                if (r) {
+                  return r;
+                }
+                return r || _this.conditionCheck([e], object, time);
+              };
+            })(this), false);
+          case 'and':
+            return condition.and.reduce((function(_this) {
+              return function(r, e) {
+                if (!r) {
+                  return r;
+                }
+                return r && _this.conditionCheck([e], object, time);
+              };
+            })(this), true);
+          case 'not':
+            return !this.conditionCheck([condition.not], object, time);
+        }
+      }
+      return flag;
+    };
+
+    Campaign.prototype.canReset = function(object, time) {
+      return this.conditionCheck(this.config.reset_condition, object, time);
+    };
+
+    Campaign.prototype.update = function(object) {
+      var thisData, _base;
+      thisData = {
+        object: object
+      };
+      return typeof (_base = this.config).update === "function" ? _base.update(thisData, utils) : void 0;
+    };
+
+    Campaign.prototype.activate = function() {
+      var delta, object, thisData, time, _base;
+      object = arguments[0];
+      delta = arguments[1];
+      time = arguments[2];
+      if (typeof delta !== 'number') {
+        time = delta;
+        delta = 1;
+      }
+      object.counters[this.config.counter.key].incr(delta, time);
+      thisData = {
+        object: object
+      };
+      return typeof (_base = this.config).activate === "function" ? _base.activate(thisData, utils) : void 0;
+    };
+
+    Campaign.prototype.isActive = function(object, time) {
+      if (this.config.storeType && object.type !== this.config.storeType) {
+        return false;
+      }
+      return this.conditionCheck(this.config.available_condition, object, time);
+    };
+
+    Campaign.prototype.reset = function(object, time) {
+      var action, thisData, _i, _len, _ref, _results;
+      if (!this.config.reset_action) {
+        return;
+      }
+      _ref = this.config.reset_action;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        action = _ref[_i];
+        switch (action.type) {
+          case 'function':
+            thisData = {
+              object: object,
+              time: time
+            };
+            _results.push(action.func(thisData, utils));
+            break;
+          default:
+            _results.push(void 0);
+        }
+      }
+      return _results;
+    };
+
+    Campaign.prototype.getInfo = function(object, time) {
+      var counter, counterConfig, counterKey, result;
+      result = {};
+      counterConfig = this.config.counter;
+      if (counterConfig) {
+        counterKey = counterConfig.key;
+        if (!object.counters[counterKey]) {
+          object.counters[counterKey] = new libCounter.Counter(counterConfig);
+        } else if (!(object.counters[counterKey] instanceof libCounter.Counter)) {
+          counter = new libCounter.Counter(counterConfig);
+          counter.counter = object.counters[counterKey].counter;
+          counter.time = object.counters[counterKey].time;
+          object.counters[counterKey] = counter;
+        }
+        result.counter = object.counters[counterKey];
+      }
+      return result;
+    };
+
+    return Campaign;
+
+  })();
+
+  exports.Campaign = Campaign;
+
+}).call(this);
